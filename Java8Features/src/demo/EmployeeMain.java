7d8ea3d80cc38a0f618afecc58f8baf9149ff2e5
RahulAher2100 <110445469+RahulAher2100@users.noreply.github.com>
2023-05-24 11:47:55 +0530

    Java 8 Features Example

---
+++ Java8Features/src/demo/EmployeeMain.java
@@ -0,0 +1,26 @@
+package demo;
+
+
+public class EmployeeMain {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+	
+
+}
+
+
+
+
+class Employee{
+	
+	private int id;
+	private String name;
+	private String address;
+	
+	
+}
